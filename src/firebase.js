19053ec5b010167f20efaa17f13c976d1d2cac38
Soham Srimany <73331793+soham0511@users.noreply.github.com>
2021-08-05 13:03:59 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 import firebase from "firebase";
 const firebaseConfig = {
-  apiKey: "AIzaSyDKxIgb5dfK-adhYbru1M-vd0v21pGC2ys",
-  authDomain: "tiktok-clone-f1186.firebaseapp.com",
-  projectId: "tiktok-clone-f1186",
-  storageBucket: "tiktok-clone-f1186.appspot.com",
-  messagingSenderId: "195165872154",
-  appId: "1:195165872154:web:b77fcc59c61bb88f11f1a3",
-  measurementId: "G-VWKN27VSYW"
+  apiKey: "X",
+  authDomain: "X",
+  projectId: "X",
+  storageBucket: "X",
+  messagingSenderId: "X",
+  appId: "X",
+  measurementId: "X"
 };
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 
